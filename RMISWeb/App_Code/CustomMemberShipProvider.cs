b430a6338eb2abe59a4ec45d0e011ef570560db3
allinonekk <nagavardhan.kj@gmail.com>
2016-05-19 21:56:30 +0530

    Added Changes
    Added Changes

---
+++ RMISWeb/App_Code/CustomMemberShipProvider.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+  public class CustomMembershipProvider : MembershipProvider
+    {
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            ValidatePasswordEventArgs args = new ValidatePasswordEventArgs(username, password, true);
+            OnValidatingPassword(args);
+
+            if (args.Cancel)
+            {
+                status = MembershipCreateStatus.InvalidPassword;
+                return null;
+            }
+
+            if (RequiresUniqueEmail && GetUserNameByEmail(email) != string.Empty)
+            {
+                status = MembershipCreateStatus.DuplicateEmail;
+                return null;
+            }
+
+            MembershipUser user = GetUser(username, true);
+
+            if (user == null)
+            {
+                //EmployeeInfo userObj = new EmployeeInfo();
+                //userObj.EmployeeId = username;
+                //userObj.Name = username;
+                //userObj.Password = Utilities.GetMD5Hash(password);
+                //userObj.WEmail = email;
+                //userObj.JoiningDate = DateTime.Now;
+                //userObj.LastModifiedDate = DateTime.Now;
+                //userObj.ObsInd = Constant.No;
+                //new EmployeeBusiness().AddOrUpdateUser(userObj);
+                //User userRep = new User();
+                //userRep.RegisterUser(userObj);
+
+                status = MembershipCreateStatus.Success;
+
+                return GetUser(username, true);
+            }
+            else
+            {
+                status = MembershipCreateStatus.DuplicateUserName;
+            }
+
+            return null;
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EnablePasswordReset
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool EnablePasswordRetrieval
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            //EmployeeBusiness userRep = new EmployeeBusiness();
+            //EmployeeInfo user = userRep.GetUserObjByUserName(username);
+            //// EmployeeDetail userDetail = userRep.GetAllEmployeeDetails().SingleOrDefault(u => u.EmployeeId == username);
+
+            //if (user != null)
+            //{
+            //    MembershipUser memUser = new MembershipUser("CustomMembershipProvider", username, user.EmployeeId, user.WEmail,
+            //                                                string.Empty, string.Empty,
+            //                                                true, false, DateTime.MinValue,
+            //                                                DateTime.MinValue,
+            //                                                DateTime.MinValue,
+            //                                                DateTime.Now, DateTime.Now);
+            //    return memUser;
+            //}
+            return null;
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get { return 6; }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get { return false; }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get { return false; }
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateUser(string username, string password)
+        {
+            //string sha1Pswd = Utilities.GetMD5Hash(password);
+            //EmployeeBusiness user = new EmployeeBusiness();
+            //EmployeeInfo userObj = user.GetUserObjByUserName(username, sha1Pswd);
+            //if (userObj != null)
+            //    return true;
+            return false;
+        }
+
+    }
---
+++ RMISWeb/App_Code/CustomRoleProvider.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+//using AI_HRnPayrollBusiness;
+
+/// <summary>
+/// Summary description for CustomRoleProvider
+/// </summary>
+public class CustomRoleProvider : RoleProvider
+{
+    public override bool IsUserInRole(string username, string roleName)
+    {
+
+        //EmployeeBusiness business = new EmployeeBusiness();
+        //return business.IsUserInRole(username, roleName);
+        throw new NotImplementedException();
+    }
+
+    public override string[] GetRolesForUser(string username)
+    {
+        //EmployeeBusiness business = new EmployeeBusiness();
+        //return business.GetRolesForUser(username);
+        throw new NotImplementedException();
+    }
+
+    // -- Snip --
+
+    public override string[] GetAllRoles()
+    {
+        //EmployeeBusiness business = new EmployeeBusiness();
+        //return business.GetAllRoles().Select(u => u.RoleName).ToArray();
+        throw new NotImplementedException();
+    }
+
+    // -- Snip --
+
+    public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+    {
+        //EmployeeBusiness business = new EmployeeBusiness();
+        //business.AddUsersToRoles(usernames, roleNames);
+        throw new NotImplementedException();
+    }
+
+    public override string ApplicationName
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public override void CreateRole(string roleName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string[] GetUsersInRole(string roleName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool RoleExists(string roleName)
+    {
+        throw new NotImplementedException();
+    }
+}
