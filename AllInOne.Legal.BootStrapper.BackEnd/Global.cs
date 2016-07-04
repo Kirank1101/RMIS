a3a628dc3d14e7a69ecb82d7c0d39394e40c7c15
Kirank1101 <Kiran.kompalla@hotmail.com>
2016-07-04 21:13:22 +0530

    session changes

--- AllInOne.Legal.BootStrapper.BackEnd/Global.cs
+++ AllInOne.Legal.BootStrapper.BackEnd/Global.cs
@@ -10,6 +10,7 @@ namespace AllInOne.BootStrapper.BackEnd
     using System.Web;
     using System.Web.Security;
     using System.Web.SessionState;
+    using System.Web.Caching;
 
     //using Microsoft.SharePoint;
     //using Microsoft.SharePoint.ApplicationRuntime;
@@ -79,6 +80,24 @@ namespace AllInOne.BootStrapper.BackEnd
            // string log4netConfigFile = ConfigurationManager.AppSettings["LogConfigFileLocation"];
             BootStrapper.ConfigLogger(string.Empty);
             BootStrapper.StartUp();
+            AddTask("DoStuff", 180);
+        }
+
+        private static CacheItemRemovedCallback OnCacheRemove = null;
+
+        private void AddTask(string name, int seconds)
+        {
+            OnCacheRemove = new CacheItemRemovedCallback(CacheItemRemoved);
+            HttpRuntime.Cache.Insert(name, seconds, null,
+                DateTime.Now.AddSeconds(seconds), Cache.NoSlidingExpiration,
+                CacheItemPriority.NotRemovable, OnCacheRemove);
+        }
+
+        public void CacheItemRemoved(string k, object v, CacheItemRemovedReason r)
+        {
+            // do stuff here if it matches our taskname, like WebRequest
+            // re-add our task so it recurs
+            AddTask(k, Convert.ToInt32(v));
         }
 
         /// <summary>
